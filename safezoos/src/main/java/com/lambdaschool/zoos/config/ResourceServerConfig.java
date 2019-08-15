31db943018db25a4c820213bf2ddaf0d5340ffce
Donald Whitely <dswhitely1@gmail.com>
2019-08-15 17:01:56 -0400

    Imported App Config Methods for OAuth2

---
+++ safezoos/src/main/java/com/lambdaschool/zoos/config/ResourceServerConfig.java
@@ -0,0 +1,5 @@
+package com.lambdaschool.zoos.config;
+
+public class ResourceServerConfig
+{
+}
---
+++ safezoos/src/main/java/com/lambdaschool/zoos/config/SimpleCorsFilter.java
@@ -0,0 +1,51 @@
+package com.lambdaschool.zoos.config;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class SimpleCorsFilter implements Filter
+{
+    public SimpleCorsFilter()
+    {
+
+    }
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException
+    {
+
+    }
+
+    @Override
+    public void destroy()
+    {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException
+    {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        if (HttpMethod.OPTIONS.name().equalsIgnoreCase(((HttpServletRequest) servletRequest).getMethod()))
+        {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else
+        {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+}
